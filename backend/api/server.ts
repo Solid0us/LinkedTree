ac4336de809a3c94ee6062995df211d4be2f0973
solid0us <blt11797@gmail.com>
2024-02-11 00:03:25 -0800

    Moved server.ts back to src

--- backend/api/server.ts
+++
@@ -1,9 +0,0 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import app from "../src/app";
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
