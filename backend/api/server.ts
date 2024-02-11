be24d6f6f0c1a17938e42e10ba90acae03ea226e
solid0us <blt11797@gmail.com>
2024-02-10 23:35:45 -0800

    removed process env checks

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -2,22 +2,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import app from "../src/app";
 
-const {
-  ACCESS_TOKEN_SECRET,
-  REFRESH_TOKEN_SECRET,
-  CORS_ORIGIN,
-  JWT_COOKIE_EXPIRES_IN,
-  DATABASE_URL,
-} = process.env;
-if (
-  !ACCESS_TOKEN_SECRET ||
-  !REFRESH_TOKEN_SECRET ||
-  !CORS_ORIGIN ||
-  !JWT_COOKIE_EXPIRES_IN ||
-  !DATABASE_URL
-)
-  throw new Error("Enivronment variables are not set up correctly.");
-
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
