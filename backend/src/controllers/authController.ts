6232f01d944134be426a5968ca86fd8b2d6cfe6f
solid0us <blt11797@gmail.com>
2024-02-11 14:16:16 -0800

    added path option to cookies

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -32,6 +32,7 @@ const createHttpOnlyCookie = (
   const cookieOptions = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     secure: true,
+    path: "/",
     httpOnly: true,
   };
   res.cookie(tokenName, jwtToken, cookieOptions);
