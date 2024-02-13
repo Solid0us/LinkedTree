8185fbc45565277b6e9c97a2f8c2519110944cfb
solid0us <blt11797@gmail.com>
2024-02-12 23:22:23 -0800

    Created side-scrolling collapsable navbar

--- frontend/src/layouts/navbar/Navbar.jsx
+++ frontend/src/layouts/navbar/Navbar.jsx
@@ -9,77 +9,101 @@ import AnchorLink from "./components/AnchorLink";
 const MainNav = () => {
   const auth = useAuth();
   const [hamburger, setHamburger] = useState(true);
-  const toggleHamburger = (e) => {
-    e.target.name = e.target.name === "menu" ? "close" : "menu";
+  const [hide, setHide] = useState(true);
+  const toggleSideNav = () => {
+    setHide((prevState) => !prevState);
+  };
+  const toggleHamburger = () => {
+    setHamburger((prevState) => !prevState);
+    toggleSideNav();
   };
   const logout = () => {
     auth.logout();
   };
-  const [hide, setHide] = useState(true);
   return (
-    <nav
-      className="flex justify-between items-center fixed top-10 left-1/2 transform -translate-x-1/2  bg-slate-200 rounded-full pl-4 pr-4 h-20 mx-auto w-[95%]"
-      onClick={() => setHide((prevState) => !prevState)}
-    >
-      {/* <div className="nav-links md:static relative md:min-h-fit min-h-[60vh] left-0 top-[-9%] md:w-auto w-full flex items-center"> */}
-      <div className="md:px-10 py-4 px-7 md:flex justify-between items-center">
-        <a href="/" className="no-underline text-black">
-          <LinkedTreeLogo />
-        </a>
-        <ul
-          className={`absolute top-full right-0 w-screen overflow-visible text-3xl bg-green-400 underline underline-offset-8 ${
-            hide ? "invisible translate-x-full" : "translate-x-0"
-          } 
+    <>
+      <nav className="flex justify-between items-center fixed top-4 left-1/2 transform -translate-x-1/2 bg-slate-200 rounded-full pl-4 pr-4 h-20 mx-auto w-[95%] z-200">
+        <div className="md:px-10  md:flex justify-between items-center">
+          <a href="/" className="no-underline text-black">
+            <LinkedTreeLogo />
+          </a>
+          <ul
+            className={`hidden lg:visible
          lg:flex lg:flex-row lg:relative lg:bg-slate-200 lg:w-full lg:no-underline lg:text-base lg:translate-x-0 transition-all duration-500 ease-in-out`}
-        >
-          <li className="pl-5">
+          >
+            <li className="pl-5">
+              <AnchorLink href="/" text="Templates" />
+            </li>
+            <li className="pl-5">
+              <AnchorLink href="/" text="Discover" />
+            </li>
+            <li className="pl-5">
+              <AnchorLink href="/" text="Pricing" />
+            </li>
+            <li className="pl-5">
+              <AnchorLink href="/" text="Learn" />
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-6">
+          <a
+            href={`${auth.user ? "/admin" : "/login"}`}
+            className="no-underline"
+          >
+            <div className="bg-slate-300 p-3 rounded-md">
+              <span className="text-black font-bold">
+                {auth.user ? "Admin" : "Log in"}
+              </span>
+            </div>
+          </a>
+          {auth.user ? (
+            <a href="/signout " className="no-underline">
+              <div className="bg-black rounded-full  p-3">
+                <span className="text-white">Logout</span>
+              </div>
+            </a>
+          ) : (
+            <a href="/signup" className="no-underline">
+              <div className="bg-black rounded-full  p-3">
+                <span className="text-white">Sign up free</span>
+              </div>
+            </a>
+          )}
+          <div className="flex lg:hidden">
+            {hamburger ? (
+              <MenuIcon className="hidden" onClick={toggleHamburger} />
+            ) : (
+              <CloseIcon className="hidden" onClick={toggleHamburger} />
+            )}
+          </div>
+        </div>
+      </nav>
+
+      <div
+        className={`fixed top-0 right-0 bg-green-400 w-screen h-screen ${
+          hide ? "translate-x-full" : "translate-x-0"
+        } transition-all duration-500 ease-in-out z-100 lg:translate-x-full`}
+      >
+        <ul className="flex flex-col mt-32 text-3xl">
+          <li className="">
             <AnchorLink href="/" text="Templates" />
           </li>
-          <li className="pl-5">
+          <div className="border-solid border border-black bg-black w-11/12 h-0.5"></div>
+          <li className="">
             <AnchorLink href="/" text="Discover" />
           </li>
-          <li className="pl-5">
+          <div className="border-solid border border-black bg-black w-11/12 h-0.5"></div>
+          <li className="">
             <AnchorLink href="/" text="Pricing" />
           </li>
-          <li className="pl-5">
+          <div className="border-solid border border-black bg-black w-11/12 h-0.5"></div>
+          <li className="">
             <AnchorLink href="/" text="Learn" />
           </li>
+          <div className="border-solid border border-black bg-black w-11/12 h-0.5"></div>
         </ul>
       </div>
-      <div className="flex items-center gap-6">
-        <a href={`${auth.user ? "/admin" : "/login"}`} className="no-underline">
-          <div className="bg-slate-300 p-3 rounded-md">
-            <span className="text-black font-bold">
-              {auth.user ? "Admin" : "Log in"}
-            </span>
-          </div>
-        </a>
-        {auth.user ? (
-          <a href="/signout " className="no-underline">
-            <div className="bg-black rounded-full  p-3">
-              <span className="text-white">Logout</span>
-            </div>
-          </a>
-        ) : (
-          <a href="/signup" className="no-underline">
-            <div className="bg-black rounded-full  p-3">
-              <span className="text-white">Sign up free</span>
-            </div>
-          </a>
-        )}
-        {hamburger ? (
-          <MenuIcon
-            className="hidden"
-            onClick={() => setHamburger((prevState) => !prevState)}
-          />
-        ) : (
-          <CloseIcon
-            className="hidden"
-            onClick={() => setHamburger((prevState) => !prevState)}
-          />
-        )}
-      </div>
-    </nav>
+    </>
   );
 };
 
--- frontend/src/layouts/navbar/components/LinkedTreeLogo.tsx
+++ frontend/src/layouts/navbar/components/LinkedTreeLogo.tsx
@@ -2,7 +2,7 @@ import ForestTwoToneIcon from "@mui/icons-material/ForestTwoTone";
 
 const LinkedTreeLogo = () => {
   return (
-    <div className="flex flex-row items-center gap-1">
+    <div className="flex flex-row items-center gap-1 p">
       <h1 className="font-sans hidden lg:inline-block">LinkedTree</h1>
       <ForestTwoToneIcon fontSize="large" color="success" />
     </div>
