a0aed80ef47bb6247aaf130c94dd7f26259a5240
solid0us <blt11797@gmail.com>
2024-02-17 13:11:12 -0800

    add blur, fixed background

--- frontend/src/Pages/admin/Admin.tsx
+++ frontend/src/Pages/admin/Admin.tsx
@@ -21,8 +21,8 @@ const Admin = () => {
     try {
       await linksServices.createLink(linkForm);
       setLinkForm({ link: "" });
-      alert("success!");
       getAllLinks();
+      setAddLinkMenu(false);
     } catch (err) {
       console.log(err);
     }
@@ -48,64 +48,74 @@ const Admin = () => {
         <LinkedTreeLogo />
       </NavBarSkeleton>
       <div className="flex flex-col md:flex-row h-screen">
-        <div className="bg-gray-100 w-full md:w-2/3 border-r pl-5 pr-5 lg:pl-10 lg:pr-10 pt-28">
-          {addLinkMenu ? (
-            <div className={`w-full h-64 rounded-xl animate-pop-in bg-gray-50`}>
-              <div className="flex justify-end p-1">
-                <div className="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200">
-                  <CloseTwoToneIcon
-                    onClick={() => setAddLinkMenu(false)}
-                    className="cursor-pointer "
-                    fontSize="small"
+        <div className="flex flex-col bg-gray-100 w-full md:w-2/3 border-r pb-2 pl-5 pr-5 lg:pl-10 lg:pr-10 pt-28 items-center">
+          <div className="w-11/12 lg:w-2/3 max-w-3xl">
+            {addLinkMenu ? (
+              <div
+                className={`w-full h-64 rounded-xl animate-pop-in bg-gray-50`}
+              >
+                <div className="flex justify-end p-1">
+                  <div className="flex items-center justify-center w-10 h-10 rounded-full hover:bg-gray-200">
+                    <CloseTwoToneIcon
+                      onClick={() => setAddLinkMenu(false)}
+                      className="cursor-pointer "
+                      fontSize="small"
+                    />
+                  </div>
+                </div>
+                <h2 className="font-bold">Enter URL</h2>
+                <div className="flex flex-row gap-4 pl-5 pr-5">
+                  <input
+                    className="bg-gray-200 rounded-md w-full caret-black p-2"
+                    placeholder="URL"
+                    value={linkForm.link}
+                    onChange={(e) =>
+                      setLinkForm((prevState) => ({
+                        ...prevState,
+                        link: e.target.value,
+                      }))
+                    }
                   />
+                  <button
+                    className=" bg-gray-300 w-20 h-14 rounded-full font-bold"
+                    onClick={handleAddLink}
+                  >
+                    Add
+                  </button>
                 </div>
               </div>
-              <h2 className="font-bold">Enter URL</h2>
-              <div className="flex flex-row gap-4 pl-5 pr-5">
-                <input
-                  className="bg-gray-200 rounded-md w-full caret-black p-2"
-                  placeholder="URL"
-                  value={linkForm.link}
-                  onChange={(e) =>
-                    setLinkForm((prevState) => ({
-                      ...prevState,
-                      link: e.target.value,
-                    }))
-                  }
-                />
-                <button
-                  className=" bg-gray-300 w-20 h-14 rounded-full font-bold"
-                  onClick={handleAddLink}
-                >
-                  Add
-                </button>
-              </div>
-            </div>
-          ) : (
-            <button
-              className={`bg-purple-700 rounded-full w-full  text-white hover:bg-purple-800 ${
-                addLinkMenu ? "invisible h-0" : "visible h-12"
+            ) : (
+              <button
+                className={`bg-purple-700 rounded-full w-full  text-white hover:bg-purple-800 ${
+                  addLinkMenu ? "invisible h-0" : "visible h-12"
+                }`}
+                onClick={() => setAddLinkMenu(true)}
+              >
+                <AddIcon type="" />
+                Add link
+              </button>
+            )}
+            <div
+              className={`flex flex-col items-center ${
+                addLinkMenu && "blur-2xs"
               }`}
-              onClick={() => setAddLinkMenu(true)}
             >
-              <AddIcon type="" />
-              Add link
-            </button>
-          )}
-          <div className="flex flex-col items-center">
-            {linkList.map((link) => {
-              return (
-                <div
-                  className="w-full h-32 bg-white rounded-xl mt-3"
-                  draggable={true}
-                >
-                  {link.link}
-                </div>
-              );
-            })}
+              {linkList.toReversed().map((link) => {
+                return (
+                  <div
+                    className="w-full h-32 bg-white rounded-2xl mt-3"
+                    draggable={true}
+                  >
+                    {link.link}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
-        <div className="bg-gray-100 hidden md:w-1/3"></div>
+        <div className="bg-gray-100 hidden md:w-1/3 md:flex md:flex-col md:justify-center md:items-center">
+          <div className="bg-black aspect-9/16 min-w-52 w-3/5 rounded"></div>
+        </div>
       </div>
     </>
   );
